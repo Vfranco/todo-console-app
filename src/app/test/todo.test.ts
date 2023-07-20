2593609ab76bf2b61ba585a1608a49cb17cbc336
William Garcés <william.garces@kuasar.co>
2023-07-20 15:13:54 -0500

    fix: fir imports and rename instances

--- src/app/test/todo.test.ts
+++ src/app/test/todo.test.ts
@@ -1,28 +1,29 @@
+import { InMemoryRepository } from "../core/data/in-memory.repository";
 import { TodoService } from "../core/services/todo.service";
-import { TodoEntitie } from "../domain/entities/todo.entitie";
+import { TaskEntity } from "../domain/entities/task.entitie";
 
 describe('App Todo Test', () => {
 
-    let todoService = new TodoService;
+    let todoService = new TodoService(new InMemoryRepository);
 
     it('create should return a boolean value', () => {
-        let newValue: TodoEntitie = {
-            taskName : "valor"
+        let newValue: TaskEntity = {
+            taskName: "valor"
         }
         expect(todoService.create(newValue)).toBe(true);
     });
 
     it('read should return a list of tasks', () => {
-        let newValue: TodoEntitie = {
-            taskName : "valor"
+        let newValue: TaskEntity = {
+            taskName: "valor"
         }
         todoService.create(newValue);
         expect(todoService.read().length).toBeGreaterThan(0);
     });
 
     it('update should update a value and return true', () => {
-        let createValue: TodoEntitie = {
-            taskName : "valor"
+        let createValue: TaskEntity = {
+            taskName: "valor"
         }
         let valueToUpdate = "nuevo valor";
 
@@ -31,8 +32,8 @@ describe('App Todo Test', () => {
     });
 
     it('delete should remove a item from list and return true', () => {
-        let createValue: TodoEntitie = {
-            taskName : "valor"
+        let createValue: TaskEntity = {
+            taskName: "valor"
         }
         todoService.create(createValue);
         expect(todoService.delete(0)).toBe(true);
